b0cb13746161277f836e8eb3b4ceb43f1e0c83d8
Adamcodi <adamcodi28@gmail.com>
2025-08-25 01:08:06 +0200

    Update tailwind.config.ts
    fix: tailwind content

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,23 +1,18 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
+
 const config: Config = {
   content: [
     "./src/**/*.{js,ts,jsx,tsx}",
     "./src/app/**/*.{js,ts,jsx,tsx}",
-    "./src/components/**/*.{js,ts,jsx,tsx}"
+    "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
       colors: {
-        emerald: {
-          50: '#ECFDF5',
-          100: '#D1FAE5',
-          200: '#A7F3D0',
-          500: '#10B981',
-          600: '#059669'
-        }
+        emerald: { 50:"#ECFDF5",100:"#D1FAE5",200:"#A7F3D0",500:"#10B981",600:"#059669" }
       }
-    }
+    },
   },
-  plugins: []
-}
-export default config
+  plugins: [],
+};
+export default config;
